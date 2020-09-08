2e021a25fe80a4f553a4ebd69480c157210eae06
danahzhang <danazhang@gmail.com>
2020-09-07 23:42:57 -0700

    update 1

--- static/sizing.js
+++ static/sizing.js
@@ -1,9 +1,11 @@
 $(window).on('resize', function() {
-    if($(window).width() < 600) {
+    if($(window).width() < 1200) {
         $('#body').removeClass('the1200');
         $('#body').addClass('the600');
+        console.log("aa");
     }else{
         $('#body').addClass('the1200');
         $('#body').removeClass('the600');}  
+        console.log("bb");
     }
 )
