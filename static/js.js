3a6529eaddf9ad2af1542078644db692fff23c6d
danahzhang <danazhang@gmail.com>
2020-09-27 21:34:53 -0700

    resume

---
+++ static/js.js
@@ -0,0 +1,9 @@
+function giveinfo(){
+    var contactinfo = "Phone: (510) 990-1146";
+    contactinfo += "<br>";
+    contactinfo += "Email: danahzhang@gmail.com";
+    contactinfo += "<br>";
+    contactinfo += "GitHub: https://github.com/danahzhang";
+    console.log(contactinfo);
+    document.getElementById("contact").innerHTML = contactinfo;
+}
